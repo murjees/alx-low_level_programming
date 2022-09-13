57a1c07c09dadb1547e2804d3480aff27aaf0fb1
murjees <murjanatu310@gmail.com>
2022-09-13 04:49:30 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
-#include "holberton.h"
+#include"main.h"
 
 /**
-* main - Print the word "Holberton"
-*
-* Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	int i;
-	char p[] = "Holberton";
+	char str[] = "_putchar";
+	int ch;
 
-	for (i = 0; i < 9; i++)
-		{
-		_putchar(p[i]);
-		}
-		_putchar('\n');
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
+
 	return (0);
 }
